5417d61d8af57e0c7763482adac57a5b377ed2d3
KaungMyatK <kaungmyat.dev8@gmail.com>
2026-08-31 01:07:17 +0630

    redesign contact us page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,11 @@
 import ContactSection from "../components/contact/ContactSection";
+import CtaSection from "../components/contact/CtaSection";
 
 function Contact() {
   return (
     <>
       <ContactSection />
+      <CtaSection />
     </>
   );
 }
